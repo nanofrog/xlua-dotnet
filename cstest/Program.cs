bb825021a9fb03dfb493536b0489919ce32bb38b
zqw10 <zqw1989@outlook.com>
2017-02-20 23:47:24 +0800

    添加了测试程序，运行成功

---
+++ cstest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XLua;
+
+namespace cstest
+{
+    [LuaCallCSharp]
+    class A
+    {
+        public float x, y;
+
+        public float add()
+        {
+            return x + y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var luaenv = new LuaEnv();
+            A a = new A() { x = 1, y = 2 };
+            luaenv.Global.Set("a", a);
+            a.x = 3;
+            luaenv.DoString("print(a:add())");
+        }
+    }
+}
