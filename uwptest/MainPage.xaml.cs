bdcf0ec7769eb2cf4754c1277c6646cfcc9505aa
zqw10 <zqw1989@outlook.com>
2017-02-22 00:53:01 +0800

    添加uwp项目，目前在utils的getassemblies函数还有点问题，会卡住，要想办法解决

---
+++ uwptest/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XLua;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace uwptest
+{
+    [LuaCallCSharp]
+    class A
+    {
+        public float x, y;
+
+        public float add()
+        {
+            return x + y;
+        }
+    }
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var luaenv = new LuaEnv();
+            A a = new A() { x = 1, y = 2 };
+            luaenv.Global.Set("a", a);
+            a.x = 3;
+            luaenv.DoString("print(a:add())");
+        }
+    }
+}
